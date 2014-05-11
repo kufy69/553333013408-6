8520300a71a89a07a2f4181147979eb6bb948d8f
kufy69 <nujued@gmail.com>
2014-05-11 19:04:36 +0700

    dasdasd

---
+++ src/Work02/Stack.java
@@ -0,0 +1,22 @@
+package Work02;
+
+public class Stack {
+
+	ArrayList list = new ArrayList();
+	public void push(Object e) {
+		list.add(e);
+	}
+	public Object pop() {
+		Object a = list.get(list.size());
+		list.remove(list.size());
+		return a;
+	}
+	public Object peek() {
+		return list.get(list.size());
+	}
+	@Override
+	public String toString() {		
+		return String.valueOf(list);
+	}
+
+}
